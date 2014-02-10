c9e733426b434954c18402ceafb7c3651f25238d
randlet <randle.taylor@gmail.com>
2014-02-10 15:39:21 -0500

    slight modification to DOM

--- listable/settings.py
+++ listable/settings.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-LISTABLE_DOM =  getattr(settings, "LISTABLE_DOM", '<"row"<"col-sm-6"ir><"col-sm-6"p>>t<"row-fluid"<"col-sm-12"lp>>')
+LISTABLE_DOM =  getattr(settings, "LISTABLE_DOM", '<"row"<"col-sm-6"i><"col-sm-6"rp>>rt<"row"<"col-sm-12"lp>>')
 LISTABLE_PAGINATION_TYPE = getattr(settings, "LISTABLE_PAGINATION_TYPE", "bootstrap")
 LISTABLE_STATE_SAVE = getattr(settings, "LISTABLE_STATE_SAVE", True)
 LISTABLE_PAGINATE_BY = getattr(settings, "LISTABLE_PAGINATE_BY", 10)
