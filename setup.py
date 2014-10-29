5e98937e8548da24b8b5e9e4b8a47099b0e6587b
Randle Taylor <randle.taylor@gmail.com>
2014-10-29 13:13:44 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@
 import os
 import sys
 
-import listable
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-version = listable.__version__
+version = '0.2.1'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
