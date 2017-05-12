5897a2de379f3ad9bc34f8d1505a0fb62eea4187
Randle Taylor <randle.taylor@gmail.com>
2017-05-11 22:34:22 -0400

    unicode fix in values_to_dt

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.4.1'
+version = '0.4.3'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
