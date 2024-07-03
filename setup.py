3ecaedea043138ab1b49e8cbc13ecb7eccff8a93
Hasan Ammar <me@hackoder.com>
2024-07-03 15:24:29 -0400

    Bump version to 0.8.3.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.8.2'
+version = '0.8.3'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
