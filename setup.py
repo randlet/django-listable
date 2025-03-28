830c91017ed2e0ae78abe5d4973c1bf6ce741226
randlet <randle.taylor@gmail.com>
2025-03-28 13:20:23 -0400

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.8.10'
+version = '0.9.0'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
